8e9f3986994df620272c6d7e455e145ae606ff7d
Dj <ritchey0713@gmail.com>
2020-03-01 16:00:19 -0500

    fix indents

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -18,11 +18,9 @@ namespace WpfApp1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
     }
+  }
 }
