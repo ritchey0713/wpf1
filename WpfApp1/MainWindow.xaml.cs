f184df16b285041db48bd928a4df2b190f1b845d
Dj <ritchey0713@gmail.com>
2020-03-01 21:34:10 -0500

    add button event

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,29 +21,36 @@ namespace WpfApp1
   public partial class MainWindow : Window {
     public MainWindow() {
       InitializeComponent();
-      Grid grid = new Grid();
-      this.Content = grid;
-      Button btn = new Button();
-      btn.FontSize = 40;
+      //Grid grid = new Grid();
+      //this.Content = grid;
+      //Button btn = new Button();
+      //btn.Height = 250;
+      //btn.Width = 500;
+      //btn.FontSize = 40;
 
-      WrapPanel wrapPanel = new WrapPanel();
-      TextBlock txt = new TextBlock();
-      txt.Text = "Multi";
-      txt.Foreground = Brushes.Blue;
-      wrapPanel.Children.Add(txt);
+      //WrapPanel wrapPanel = new WrapPanel();
+      //TextBlock txt = new TextBlock();
+      //txt.Text = "Multi";
+      //txt.Foreground = Brushes.Blue;
+      //wrapPanel.Children.Add(txt);
 
-      TextBlock txt2 = new TextBlock();
-      txt2.Text = "Color";
-      txt2.Foreground = Brushes.Red;
-      wrapPanel.Children.Add(txt2);
+      //TextBlock txt2 = new TextBlock();
+      //txt2.Text = "Color";
+      //txt2.Foreground = Brushes.Red;
+      //wrapPanel.Children.Add(txt2);
 
-      TextBlock txt3 = new TextBlock();
-      txt3.Text = "Button";
-      txt3.Foreground = Brushes.Green;
-      wrapPanel.Children.Add(txt3);
+      //TextBlock txt3 = new TextBlock();
+      //txt3.Text = "Button";
+      //txt3.Foreground = Brushes.Green;
+      //wrapPanel.Children.Add(txt3);
 
-      btn.Content = wrapPanel;
-      grid.Children.Add(btn);
+      //btn.Content = wrapPanel;
+      //grid.Children.Add(btn);
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e) {
+      MessageBox.Show("Thanks for finding me!");
+
     }
   }
 }
