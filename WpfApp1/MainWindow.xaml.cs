414292d1780aa29c3e1516ec0404d35a52a94eb3
Dj <ritchey0713@gmail.com>
2020-03-01 19:04:43 -0500

    build grid code behind logic

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,6 +21,14 @@ namespace WpfApp1
   public partial class MainWindow : Window {
     public MainWindow() {
       InitializeComponent();
+      Grid grid = new Grid();
+      this.Content = grid;
+      Button btn = new Button();
+      btn.FontSize = 40;
+
+      WrapPanel wrapPanel = new WrapPanel();
+      TextBlock txt = new TextBlock();
+      txt.Text = "Multi";
     }
   }
 }
